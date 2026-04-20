0b7d73d33c7a211a6335d4d478a8112b901a2eac
Slavomir <Slavomir7115@gmail.com>
2026-04-20 15:32:57 +0200

    ...

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -8,56 +8,72 @@ const Form = () => {
       name="kontaktny-formular"
       method="POST"
       action="/dakujeme"
-      data-netlify="true"
       data-netlify-honeypot="bot-field"
+      data-netlify="true"
       className="bg-cards border border-border p-6 rounded-lg shadow-md w-full mx-auto"
     >
-      {/* Netlify hidden input */}
       <input type="hidden" name="form-name" value="kontaktny-formular" />
 
-      {/* honeypot */}
       <p className="hidden">
         <label>
           Toto pole nevypĺňajte: <input name="bot-field" />
         </label>
       </p>
 
-      <span className="flex flex-row items-center gap-2 mb-4">
+      <span className="flex flex-row items-center justify-start gap-2 mb-4 ">
         <Calendar className="text-primary" size={24} />
-        <h2 className="text-2xl font-bold text-foreground">Kontaktujte nás</h2>
+        <h2 className="text-2xl font-bold text-foreground ">Kontaktujte nás</h2>
       </span>
 
       <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Meno</label>
+        <label
+          htmlFor="name"
+          className="block text-sm font-medium text-foreground mb-1"
+        >
+          Meno
+        </label>
         <input
           type="text"
+          id="name"
           name="name"
           required
-          className="w-full p-2 border rounded"
+          className="w-full p-2 border border-border rounded focus:outline-none focus:ring-2 focus:ring-primary"
           placeholder="Vaše meno"
         />
       </div>
 
       <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Email</label>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-foreground mb-1"
+        >
+          Email
+        </label>
         <input
           type="email"
+          id="email"
           name="email"
           required
-          className="w-full p-2 border rounded"
+          className="w-full p-2 border border-border rounded focus:outline-none focus:ring-2 focus:ring-primary"
           placeholder="Váš email"
         />
       </div>
 
       <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Správa</label>
+        <label
+          htmlFor="message"
+          className="block text-sm font-medium text-foreground mb-1"
+        >
+          Správa
+        </label>
         <textarea
+          id="message"
           name="message"
           rows="4"
           required
-          className="w-full p-2 border rounded"
+          className="w-full p-2 border border-border rounded focus:outline-none focus:ring-2 focus:ring-primary"
           placeholder="Vaša správa"
-        />
+        ></textarea>
       </div>
 
       <PrimaryButton type="submit">Odoslať</PrimaryButton>
